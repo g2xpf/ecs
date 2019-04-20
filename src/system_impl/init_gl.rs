f7c0ef7950683d97ffffda6c5523f8da0570fd86
g2_xpf <nameko.jack@gmail.com>
2019-04-20 23:25:39 +0900

    set window title and size

--- src/system_impl/init_gl.rs
+++ src/system_impl/init_gl.rs
@@ -1,6 +1,7 @@
 use crate::types::component_type;
 use crate::types::system::System;
 use glium;
+use glium::glutin;
 use glsl_linalg::float;
 
 pub fn init_gl<F>() -> System<F>
@@ -9,7 +10,9 @@ where
 {
     |_entity, component_data| {
         let events_loop = glium::glutin::EventsLoop::new();
-        let window = glium::glutin::WindowBuilder::new();
+        let window = glium::glutin::WindowBuilder::new()
+            .with_title("GLWindow")
+            .with_dimensions(glutin::dpi::LogicalSize::new(640.0, 640.0));
         let context = glium::glutin::ContextBuilder::new().with_vsync(true);
         let display = glium::Display::new(window, context, &events_loop).unwrap();
         component_data.event_handler = Some(Box::new(component_type::EventHandler { events_loop }));
